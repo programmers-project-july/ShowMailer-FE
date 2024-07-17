4bab330748eecb777c30b3c02c3ccafb4a7d21b0
dah <cdh010126r@gmail.com>
2024-07-17 17:28:50 +0900

    feat: 홈화면 구성

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 
-import '@/pages/home/home.css';
-import Header from '@/components/header/header';
+import '@/pages/home//Home.css';
+import Header from '@/components/header/Header';
+import Filter from '@/components/Filter';
+import Content from '@/components/Content';
 
-const home = () => {
+const Home = () => {
   return (
-    <div className="appContainer">
+    <>
       <Header />
-      <div className="banner">소개</div>
-      <div className="container">
-        <div className="filter"> 필터, 검색 </div>
-        <div className="content"></div>
+      <div className="banner">
+        서울시에서 열리는
+        <br />
+        <span className="highlight">공연, 전시, 콘서트, 연극</span>까지!
+        <br />
+        한번에 모아서 확인하고,
+        <br />
+        <span className="highlight">좋아요</span>로 <span className="highlight">이메일 알림</span> 받고!
+        <br />
+        <span className="highlight">캘린더 일정</span>까지
+        <br />
+        한번에 <span className="highlight">~~~</span>로 관리해요!
       </div>
-    </div>
+      <div className="container">
+        <Filter />
+        <Content />
+      </div>
+    </>
   );
 };
 
-export default home;
+export default Home;
