415b53f6b6ad8f61acd6259a42f46e5953eb89ac
dah <cdh010126r@gmail.com>
2024-07-18 01:45:59 +0900

    feat: 파비콘, 제목, env 수정

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,17 +1,27 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { getApp, initializeApp, FirebaseApp } from 'firebase/app';
+import 'firebase/auth';
+
+export let app: FirebaseApp;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDcadd7ivQ5iEggc2nLrt2aZM2srndJoPg',
-  authDomain: 'dev-alarm.firebaseapp.com',
-  projectId: 'dev-alarm',
-  storageBucket: 'dev-alarm.appspot.com',
-  messagingSenderId: '335723868881',
-  appId: '1:335723868881:web:c49f703e0350ae59d98bf0',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+try {
+  app = getApp('app');
+} catch (e) {
+  app = initializeApp(firebaseConfig, 'app');
+}
+
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
