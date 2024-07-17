48bdb787e18ec274fa46bc5016fb5f6973d70e15
dah <cdh010126r@gmail.com>
2024-07-17 17:29:51 +0900

    feat: detail 페이지 초기구현

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,24 +1,25 @@
-import Home from "@/pages/home/home";
-import Detail from "@/pages/detail/detail";
-import Mypage from "@/pages/mypage/mypage";
-import NotFound from "@/pages/not-found";
-import { createBrowserRouter } from "react-router-dom";
+import Home from '@/pages/home/Home';
+import Detail from '@/pages/detail/Detail';
+import Mypage from '@/pages/mypage/Mypage';
+import NotFound from '@/pages/Not-found';
+import { createBrowserRouter } from 'react-router-dom';
+import { performances, Tperformance } from '@/components/Content';
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Home />,
   },
   {
-    path: "/detail",
-    element: <Detail />,
+    path: '/detail',
+    element: <Detail />
   },
   {
-    path: ":userId",
+    path: '/:userId',
     element: <Mypage />,
   },
   {
-    path: "*", // 모든 경로에 부합하지 않는 경우
-    element: <NotFound />, // NotFound 컴포넌트 렌더링
+    path: '*', // 모든 경로에 부합하지 않는 경우
+    element: <NotFound />,
   },
 ]);
