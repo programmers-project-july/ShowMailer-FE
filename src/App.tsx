420c4df9b455e4434fbf03d65501d96d080526b1
dah <cdh010126r@gmail.com>
2024-07-17 17:27:28 +0900

    feat. 모바일뷰 전역스타일

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import '@/App.css';
+import BgImg from '@/assets/imgs/BgImg.png';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+      <div className="pcOverlay">
+        <img src={BgImg} className="pcImage" alt="PC Description" />
+        <div className="appContainer">
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
+        </div>
+      </div>
     </>
   );
 }
