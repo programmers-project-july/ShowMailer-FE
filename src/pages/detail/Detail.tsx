07693cc92cdbabba6f66223236d61181c6997911
dah <cdh010126r@gmail.com>
2024-07-20 02:34:43 +0900

    feat: Detail 페이지 구성

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -1,35 +1,49 @@
 import React from 'react';
 import '@/pages/detail/Detail.css';
-import { useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Header from '@/components/header/Header';
-import { AiFillHeart  } from 'react-icons/ai';
-import { AiOutlineHeart   } from 'react-icons/ai';
+import { AiFillHeart } from 'react-icons/ai';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { usePerformances } from '@/hooks/usePerformances';
 
 const Detail: React.FC = () => {
+  const performance = usePerformances();
+
+  const handleButtonClick = () => {
+    // if (performance.ORG_LINK)
+    //   window.open(performance.ORG_LINK, '_blank');
+    alert('홈페이지 이동');
+  };
+
   return (
     <>
       <Header />
-      <div className="full-container">
-        <div className="detail-container">
-          <img
-            className="Poster"
-            src="https://culture.seoul.go.kr/cmmn/file/getImage.do?atchFileId=a6cb4e5a9b5b404e83454b084d88a399&thumb=Y"
-            alt="Poster"
-          />
-          <div className="event-text">
-            <h2>공연제목</h2>
-            <AiOutlineHeart />
-            <p>카테고리: </p>
-            <p>기간: </p>
-            <p>장소: </p>
-            <p>시간: </p>
-            <p>이용대상: </p>
-            <p>이용요금: </p>
+      <div className="detailContainer">
+        <img
+          className="Poster"
+          src="https://culture.seoul.go.kr/cmmn/file/getImage.do?atchFileId=a6cb4e5a9b5b404e83454b084d88a399&thumb=Y"
+          alt="Poster"
+        />
+        <div className="eventText">
+          <h2>오페라 갈라</h2>
+          <div className="heartContainer">
+            <AiOutlineHeart className="heartIcon" />
+            <span>좋아요 이메일 알림받기</span>
           </div>
+          <p>카테고리: </p>
+          <p>기간: </p>
+          <p>장소: </p>
+          <p>시간: </p>
+          <p>이용대상: </p>
+          <p>이용요금: </p>
         </div>
-        <button type="button">홈페이지 바로 가기</button>
       </div>
+      <button type="button" className="goPageBtn" onClick={handleButtonClick}>
+        홈페이지 바로 가기
+      </button>
+      {/* {performance.ORG_LINK} */}
+      
     </>
   );
 };
